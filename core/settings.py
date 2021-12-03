d3ad7c6ed1e3937354747bba58a1b21ce69be5d8
khava <havarg96@gmail.com>
2021-12-04 02:38:06 +0300

    front

--- core/settings.py
+++ core/settings.py
@@ -123,13 +123,21 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
